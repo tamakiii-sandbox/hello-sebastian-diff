4415d2ba73d25932a8e58a481a65ca17f51b69d8
tamakiii <tamakiii@users.noreply.github.com>
2020-06-25 15:33:20 +0900

    using $(foreach) and mount ~/.composer

--- docker.mk
+++ docker.mk
@@ -1,6 +1,8 @@
 .PHONY: help install build bash clean
 
 NAME := hello-sebastian-diff
+WORKDIR := /work
+VOLUMES := $$(pwd):$(WORKDIR) ~/.composer:/root/.composer
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -12,7 +14,7 @@ build:
 	docker build -t $(NAME) .
 
 bash:
-	docker run -it --rm -w /work -v $$(pwd):/work $(NAME) $@
+	docker run -it --rm -w $(WORKDIR) $(foreach v,$(VOLUMES),-v $v) $(NAME) $@
 
 clean:
 	docker image rm $(NAME)
